f5b14f9656639276c6cccb3e7e1f7ad597ad277c
jpmlyr <jpmlyr@gmail.com>
2016-02-01 23:58:11 +0900

    Thread.sleep()실습

---
+++ src/main/java/introduction01/Main1_9.java
@@ -0,0 +1,12 @@
+package introduction01;
+
+public class Main1_9 {
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		for(int i=0;i<10000;i++){
+			System.out.println("Good!");
+			Thread.sleep(100);
+		}
+	}
+}
