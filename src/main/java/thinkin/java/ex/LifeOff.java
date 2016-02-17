1f41842ba75e1df287f363b9a1dc5b5d49f0e220
jpmlyr <jpmlyr@gmail.com>
2016-02-18 00:48:40 +0900

    .

--- src/main/java/thinkin/java/ex/LifeOff.java
+++ src/main/java/thinkin/java/ex/LifeOff.java
@@ -2,11 +2,28 @@ package thinkin.java.ex;
 
 public class LifeOff implements Runnable {
 
-	protected int countDown;
+	protected int countDown = 10;
+	private static int taskCount = 0;
+	private final int id = taskCount++;
+
+	public LifeOff() {
+	}
+
+	public LifeOff(int countDown) {
+		this.countDown = countDown;
+	}
+	
+	public String status() {
+		return "#" + id + "(" + (countDown > 0 ? countDown : "Liftoff!")
+				+ "), ";
+	}
 
 	@Override
 	public void run() {
-
+		while(countDown-- > 0){
+			System.out.println(status());
+			Thread.yield();
+		}
 	}
 
 }
